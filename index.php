72bb87d0cf057162b02feda44bf091994c3eaa51
deusyvan <deusyvan@gmail.com>
2018-06-01 00:41:35 -0300

    Acesso no banco e criando a tabela

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php
+
+try {
+    $pdo = new PDO("mysql:dbname=blog;host=localhost","admin","admin");
+} catch (PDOException $e) {
+    echo "ERRO: ".$e->getMessage();
+    exit;
+}
+
+?>
+
+<table border="1" width=400>
+  <tr>
+    <th>Nome</th>
+    <th>Idade</th>
+  </tr>
+  
+  <?php 
+    $sql = "SELECT * FROM usuarios";
+    
+  ?>
+  <tr>
+    <td>Row 1: Col 1</td>
+    <td>Row 1: Col 2</td>
+  </tr>
+</table>
+
