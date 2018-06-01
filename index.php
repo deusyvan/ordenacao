7f65d52557a97d245018afb06f4ca2e63c1873a2
deusyvan <deusyvan@gmail.com>
2018-06-01 02:41:33 -0300

    Mudando a interação para anes do formulario para podermos deixar selecionado a opção

--- index.php
+++ index.php
@@ -7,6 +7,13 @@ try {
     exit;
 }
 
+if (isset($_GET['ordem']) && empty($_GET['ordem']) == FALSE){
+      $ordem = addslashes($_GET['ordem']);
+      $sql = "SELECT * FROM usuarios ORDER BY ".$ordem;
+  } else {
+      $ordem = '';
+      $sql = "SELECT * FROM usuarios";
+  }
 ?>
 <form method="GET">
 	<select name="ordem" onchange="this.form.submit()">
@@ -22,12 +29,6 @@ try {
   </tr>
   
   <?php 
-  if (isset($_GET['ordem']) && empty($_GET['ordem']) == FALSE){
-      $ordem = addslashes($_GET['ordem']);
-      $sql = "SELECT * FROM usuarios ORDER BY ".$ordem;
-  } else {
-       $sql = "SELECT * FROM usuarios";
-  }
     $sql = $pdo->query($sql);
     if ($sql->rowCount() > 0){
         foreach ($sql->fetchAll() as $usuario):    
